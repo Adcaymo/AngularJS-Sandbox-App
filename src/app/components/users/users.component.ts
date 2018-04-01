d7ef0326e8206ec54f839e0276be4130eb43102a
Adrian <adcaymo@gmail.com>
2018-03-31 19:35:32 -0700

    style binding & ngStyle

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -13,6 +13,7 @@ export class UsersComponent implements OnInit {
   loaded: boolean;
   enableAdd: boolean;
   currentClasses: {};
+  currentStyles: {};
 
   constructor() { }
 
@@ -66,6 +67,7 @@ export class UsersComponent implements OnInit {
     // });
 
     this.setCurrentClasses();
+    this.setCurrentStyles();
   }
 
   addUser(user: User) {
@@ -76,6 +78,13 @@ export class UsersComponent implements OnInit {
     this.currentClasses = {
       'btn-success': this.enableAdd,
       'big-text': this.showExtended
+    };
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'padding-top': this.showExtended ? '0' : '40px',
+      'font-size': this.showExtended ? '' : '40px'
     };
   }
 }
