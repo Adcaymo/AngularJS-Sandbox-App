9fccf9da2d823609de972710a0bb00b8845cbee2
Adrian <adcaymo@gmail.com>
2018-04-02 22:31:32 -0700

    http client post request

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -6,6 +6,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Post } from './../models/Post';
 
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
 @Injectable()
 export class PostService {
   postsUrl: string;
@@ -16,5 +20,9 @@ export class PostService {
 
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.postsUrl);
+  }
+
+  savePost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.postsUrl, post, httpOptions);
   }
 }
