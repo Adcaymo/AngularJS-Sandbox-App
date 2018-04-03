cf508e01f9159514e1b7d69ee3bcc31b47fa142c
Adrian <adcaymo@gmail.com>
2018-04-03 00:05:50 -0700

    http client delete request

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -18,17 +18,24 @@ export class PostService {
     this.postsUrl = 'https://jsonplaceholder.typicode.com/posts';
   }
 
-  getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(this.postsUrl);
-  }
-
   savePost(post: Post): Observable<Post> {
     return this.http.post<Post>(this.postsUrl, post, httpOptions);
+  }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.postsUrl);
   }
 
   updatePost(post: Post): Observable<Post> {
     const url = `${this.postsUrl}/${post.id}`;
 
     return this.http.put<Post>(url, post, httpOptions);
+  }
+
+  removePost(post: Post | number): Observable<Post> {
+    const id = typeof post === 'number' ? post : post.id;
+    const url = `${this.postsUrl}/${id}`;
+
+    return this.http.delete<Post>(url, httpOptions);
   }
 }
